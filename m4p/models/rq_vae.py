4c21437e3ed75a78e2b032af380fd9789ab3441d
shaoniandugufei <dugufei@bjtu.edu.cn>
2024-08-27 00:38:49 +0800

    sm

---
+++ m4p/models/rq_vae.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+"""
+https://arxiv.org/abs/2305.05065
+"""
+
+import torch
+from torch import nn
+from einops import repeat
+from einops.layers.torch import Rearrange
+from m4p.models.vit import VIT
+
+class RQVAE(nn.Module):
+    def __init__(self, image_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0.0, emb_dropout = 0.0):
+        super().__init__()
+        self.image_encoder = None
+        self.text_encoder = None
+
+    def forward(self, text_x, img_x):
+        pass
