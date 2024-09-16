1fdf7efaa1e31da2919b517fd5a638e3357b2a21
liutengfei04 <liutengfei04@baidu.com>
2024-09-16 20:07:15 +0800

    init

--- m4p/models/attentions/__init__.py
+++ m4p/models/attentions/__init__.py
@@ -2,5 +2,7 @@
 # coding=utf-8
 
 from m4p.models.attentions.self_attention import SelfAttention
+from m4p.models.attentions.cross_attention import CrossAttention
 from m4p.models.attentions.multi_head_attention import MultiHeadAttention
 from m4p.models.attentions.causal_self_attention import CausalSelfAttention
+from m4p.models.attentions.grouped_query_self_attention import  GroupedQuerySelfAttention
